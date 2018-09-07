b5ce7edd6b04a0548a46519b3e6400b5acc85121
Owen Andronicus Martin <owen@enkaizen.com>
2018-09-07 12:12:01 +0600

    Added recipe UI

--- Forkify/src/js/index.js
+++ Forkify/src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 /** Global state of the app
@@ -75,6 +76,8 @@ const controlRecipe = async () => {
 
     if (id) {
         // Prepare the UI for changes
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
 
         // Create new recipe object
         state.recipe  = new Recipe(id);
@@ -91,7 +94,9 @@ const controlRecipe = async () => {
             state.recipe.calcServings();
 
             // Render recipe
-            console.log(state.recipe);
+            // console.log(state.recipe);
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
         } catch (err) {
             console.log(err);
         }
